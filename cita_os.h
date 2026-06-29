d3e8e6139605e3fd6fc56d946a624012a30fb7f4
Photosounder <contact@photosounder.com>
2026-06-29 08:25:00 +0200

    Added Wasm

--- cita_os.h
+++ cita_os.h
@@ -2,44 +2,42 @@
 #ifndef H_CITA_OS
 #define H_CITA_OS
 
-  #if defined(CITA_OS_WINDOWS) && defined(CITA_OS_MAC)
-    #error Define only one CITA_OS_* platform selector
+  #if defined(__wasm__)
+    #define CITA_OS_WASM
+  #elif defined(_WIN32)
+    #define CITA_OS_WINDOWS
+  #elif defined(__APPLE__) && defined(__MACH__)
+    #define CITA_OS_MAC
+  #else
+    #error cita_os.h only supports Windows, macOS and WebAssembly
   #endif
 
-  #if !defined(CITA_OS_WINDOWS) && !defined(CITA_OS_MAC)
-    #if defined(_WIN32)
-      #define CITA_OS_WINDOWS
-    #elif defined(__APPLE__) && defined(__MACH__)
-      #define CITA_OS_MAC
-    #else
-      #error cita_os.h only supports Windows and macOS
+  #ifdef CITA_OS_WASM
+    #if defined(CITA_OS_IMPLEMENTATION_PART1) && !defined(CITA_WASM_IMPLEMENTATION_PART1)
+      #define CITA_WASM_IMPLEMENTATION_PART1
     #endif
+    #if defined(CITA_OS_IMPLEMENTATION) && !defined(CITA_WASM_IMPLEMENTATION_PART2)
+      #define CITA_WASM_IMPLEMENTATION_PART2
+    #endif
+    #include "cita_wasm.h"
   #endif
 
   #ifdef CITA_OS_WINDOWS
-    #ifdef CITA_OS_IMPLEMENTATION
-      #ifndef CITA_WIN_IMPLEMENTATION
-        #define CITA_WIN_IMPLEMENTATION
-      #endif
+    #if defined(CITA_OS_IMPLEMENTATION) && !defined(CITA_WIN_IMPLEMENTATION)
+      #define CITA_WIN_IMPLEMENTATION
     #endif
-    #ifdef CITA_OS_MAX
-      #ifndef CITA_WIN_MAX
-        #define CITA_WIN_MAX CITA_OS_MAX
-      #endif
+    #if defined(CITA_OS_MAX) && !defined(CITA_WIN_MAX)
+      #define CITA_WIN_MAX CITA_OS_MAX
     #endif
     #include "cita_windows.h"
   #endif
 
   #ifdef CITA_OS_MAC
-    #ifdef CITA_OS_IMPLEMENTATION
-      #ifndef CITA_MAC_IMPLEMENTATION
-        #define CITA_MAC_IMPLEMENTATION
-      #endif
+    #if defined(CITA_OS_IMPLEMENTATION) && !defined(CITA_MAC_IMPLEMENTATION)
+      #define CITA_MAC_IMPLEMENTATION
     #endif
-    #ifdef CITA_OS_MAX
-      #ifndef CITA_MAC_MAX
-        #define CITA_MAC_MAX CITA_OS_MAX
-      #endif
+    #if defined(CITA_OS_MAX) && !defined(CITA_MAC_MAX)
+      #define CITA_MAC_MAX CITA_OS_MAX
     #endif
     #include "cita_mac.h"
   #endif
